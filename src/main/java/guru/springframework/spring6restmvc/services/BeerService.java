c50dc0e10c59e69466dab000fae7b3f92bc951f9
Leonie van Rijn <leonie.van.rijn@rabobank.nl>
2025-02-06 16:51:50 +0100

    feat: rest patch

--- src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -15,5 +15,7 @@ public interface BeerService {
 
   void updateBeerById(UUID beerId, Beer beer);
 
-  void deleteById(UUID beerId);
+  void deleteBeerById(UUID beerId);
+
+  void patchBeerById(UUID beerId, Beer beer);
 }
