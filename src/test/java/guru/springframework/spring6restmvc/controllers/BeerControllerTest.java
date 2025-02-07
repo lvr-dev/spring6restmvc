a704a7d6436fa4bc50a58ad89e1f591734421c0e
Leonie van Rijn <leonie.van.rijn@rabobank.nl>
2025-02-07 16:00:43 +0100

    feat: tests for post and put

--- src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerTest.java
@@ -10,12 +10,15 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.UUID;
+
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 import guru.springframework.spring6restmvc.models.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
@@ -68,18 +71,30 @@ public class BeerControllerTest {
   }
 
   @Test
-    void testCreateNewBeer() throws Exception {
-        Beer beer = beerServiceImpl.listBeers().get(0);
-        beer.setVersion(null);
-        beer.setId(null);
+  void testCreateNewBeer() throws Exception {
+      Beer beer = beerServiceImpl.listBeers().get(0);
+      beer.setVersion(null);
+      beer.setId(null);
 
-        given(beerService.saveNewBeer(any(Beer.class))).willReturn(beerServiceImpl.listBeers().get(1));
+      given(beerService.saveNewBeer(any(Beer.class))).willReturn(beerServiceImpl.listBeers().get(1));
 
-        mockMvc.perform(post("/api/v1/beers")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(beer)))
-                .andExpect(status().isCreated())
-                .andExpect(header().exists("Location"));
+      mockMvc.perform(post("/api/v1/beers")
+              .accept(MediaType.APPLICATION_JSON)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(beer)))
+              .andExpect(status().isCreated())
+              .andExpect(header().exists("Location"));
     }
+
+  @Test
+  void testUpdateBeer() throws Exception {
+    Beer beer = beerServiceImpl.listBeers().get(0);
+
+    mockMvc.perform(put("/api/v1/beers/" + beer.getId())
+      .accept(MediaType.APPLICATION_JSON)
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(objectMapper.writeValueAsString(beer)));
+
+    verify(beerService).updateBeerById(any(UUID.class), any(Beer.class));
+  }
 }
--- src/test/java/guru/springframework/spring6restmvc/controllers/CustomerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controllers/CustomerControllerTest.java
@@ -10,17 +10,21 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.UUID;
+
 import static org.hamcrest.core.Is.is;
 
+import guru.springframework.spring6restmvc.models.Beer;
 import guru.springframework.spring6restmvc.models.Customer;
 import guru.springframework.spring6restmvc.services.CustomerService;
 import guru.springframework.spring6restmvc.services.CustomerServiceImp;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @WebMvcTest(CustomerController.class)
 public class CustomerControllerTest {
@@ -66,20 +70,32 @@ public class CustomerControllerTest {
       .andExpect(jsonPath("$.length()", is(3)));
   }
 
+  @Test
+  void testHandlePost() throws Exception {
+      Customer customer = customerServiceImpl.listCustomers().get(0);
+      customer.setVersion(null);
+      customer.setId(null);
+
+      given(customerService.saveNewCustomer(any(Customer.class))).willReturn(customerServiceImpl.listCustomers().get(1));
+
+      mockMvc.perform(post("/api/v1/customers")
+              .accept(MediaType.APPLICATION_JSON)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(customer)))
+              .andExpect(status().isCreated())
+              .andExpect(header().exists("Location"));
+  }
+
     @Test
-    void testHandlePost() throws Exception {
-        Customer customer = customerServiceImpl.listCustomers().get(0);
-        customer.setVersion(null);
-        customer.setId(null);
+    void testHandlePut() throws Exception {
+      Customer customer = customerServiceImpl.listCustomers().get(0);
 
-        given(customerService.saveNewCustomer(any(Customer.class))).willReturn(customerServiceImpl.listCustomers().get(1));
+      mockMvc.perform(put("/api/v1/customers/" + customer.getId())
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(customer)));
 
-        mockMvc.perform(post("/api/v1/customers")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(customer)))
-                .andExpect(status().isCreated())
-                .andExpect(header().exists("Location"));
+      verify(customerService).updateCustomer(any(UUID.class), any(Customer.class));
     }
 
 
